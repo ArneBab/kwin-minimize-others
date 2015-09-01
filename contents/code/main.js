e06d62f1553a1390b22f29d17d4cbab22c391815
Karl Grasegger <kgrasegger@localhost.localdomain>
2015-09-01 09:34:09 +0200

    Changed the description for the keyboard shortcut too.

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,13 +1,13 @@
-registerShortcut("Hide all others",
-		"Hide all others",
+registerShortcut("Minizime all but active.",
+		"Minizime all but active.",
 		"Meta+Ctrl+.",
 		function() {
-		var clients = workspace.clientList();
-		for (var i=0; i<clients.length; i++) {
-		if( true != clients[i].active ) 
-		{
-		clients[i].minimized = true;
-		}
-		}
+			var clients = workspace.clientList();
+			for (var i=0; i<clients.length; i++) {
+				if( true != clients[i].active ) 
+				{
+					clients[i].minimized = true;
+				}
+			}
 		});
 
